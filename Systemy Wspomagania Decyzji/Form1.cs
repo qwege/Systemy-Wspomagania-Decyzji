5966d296450c591057b91014b882169df4e8b5a1
qwege <tomek.baczyk@o2.pl>
2023-03-06 19:46:13 +0100

    Add Load Data

--- Systemy Wspomagania Decyzji/Form1.cs
+++ Systemy Wspomagania Decyzji/Form1.cs
@@ -30,38 +30,40 @@ namespace Systemy_Wspomagania_Decyzji
 
         }
 
-        
+
 
         private void initTools()
         {
             Font font = new Font("myFont", 14, FontStyle.Italic);
 
             Button loadTextData = new Button();
-            loadTextData.BackColor= Color.White;
+            loadTextData.Click += loadtxt;
+            loadTextData.BackColor = Color.White;
             loadTextData.Text = "Load Text Data";
             loadTextData.SetBounds(Convert.ToInt32(tools.Width * 0.005), Convert.ToInt32(tools.Height * 0.05), Convert.ToInt32(tools.Width * 0.09), Convert.ToInt32(tools.Height * 0.8));
-            loadTextData.Font= font;
+            loadTextData.Font = font;
             tools.Controls.Add(loadTextData);
 
             Button loadExcelData = new Button();
-            loadExcelData.BackColor= Color.White;
+            loadExcelData.Click += loadExcel;
+            loadExcelData.BackColor = Color.White;
             loadExcelData.Text = "Load Excel Data";
             loadExcelData.SetBounds(Convert.ToInt32(tools.Width * 0.095), Convert.ToInt32(tools.Height * 0.05), Convert.ToInt32(tools.Width * 0.09), Convert.ToInt32(tools.Height * 0.8));
-            loadExcelData.Font= font;
+            loadExcelData.Font = font;
             tools.Controls.Add(loadExcelData);
 
             Button textToDigits = new Button();
-            textToDigits.BackColor= Color.White;
+            textToDigits.BackColor = Color.White;
             textToDigits.Text = "Text To Digits";
             textToDigits.SetBounds(Convert.ToInt32(tools.Width * 0.185), Convert.ToInt32(tools.Height * 0.05), Convert.ToInt32(tools.Width * 0.09), Convert.ToInt32(tools.Height * 0.8));
-            textToDigits.Font= font;
+            textToDigits.Font = font;
             tools.Controls.Add(textToDigits);
 
             Button discretize = new Button();
             discretize.BackColor = Color.White;
             discretize.Text = "Discretize";
             discretize.SetBounds(Convert.ToInt32(tools.Width * 0.275), Convert.ToInt32(tools.Height * 0.05), Convert.ToInt32(tools.Width * 0.09), Convert.ToInt32(tools.Height * 0.8));
-            discretize.Font= font;
+            discretize.Font = font;
             tools.Controls.Add(discretize);
 
 
@@ -69,55 +71,58 @@ namespace Systemy_Wspomagania_Decyzji
             standardization.BackColor = Color.White;
             standardization.Text = "Standardization";
             standardization.SetBounds(Convert.ToInt32(tools.Width * 0.365), Convert.ToInt32(tools.Height * 0.05), Convert.ToInt32(tools.Width * 0.09), Convert.ToInt32(tools.Height * 0.8));
-            standardization.Font= font;
+            standardization.Font = font;
             tools.Controls.Add(standardization);
 
             Button changeRange = new Button();
             changeRange.BackColor = Color.White;
             changeRange.Text = "Change Range";
             changeRange.SetBounds(Convert.ToInt32(tools.Width * 0.455), Convert.ToInt32(tools.Height * 0.05), Convert.ToInt32(tools.Width * 0.09), Convert.ToInt32(tools.Height * 0.8));
-            changeRange.Font= font;
+            changeRange.Font = font;
             tools.Controls.Add(changeRange);
 
             Button showFiltered = new Button();
             showFiltered.BackColor = Color.White;
             showFiltered.Text = "Show Filtered";
             showFiltered.SetBounds(Convert.ToInt32(tools.Width * 0.545), Convert.ToInt32(tools.Height * 0.05), Convert.ToInt32(tools.Width * 0.09), Convert.ToInt32(tools.Height * 0.8));
-            showFiltered.Font= font;
+            showFiltered.Font = font;
             tools.Controls.Add(showFiltered);
 
             Button diagram2D = new Button();
             diagram2D.BackColor = Color.White;
             diagram2D.Text = "Show 2D Diagram";
             diagram2D.SetBounds(Convert.ToInt32(tools.Width * 0.635), Convert.ToInt32(tools.Height * 0.05), Convert.ToInt32(tools.Width * 0.09), Convert.ToInt32(tools.Height * 0.8));
-            diagram2D.Font= font;
+            diagram2D.Font = font;
             tools.Controls.Add(diagram2D);
 
             Button diagram3D = new Button();
             diagram3D.BackColor = Color.White;
             diagram3D.Text = "Show 3D Diagram";
             diagram3D.SetBounds(Convert.ToInt32(tools.Width * 0.725), Convert.ToInt32(tools.Height * 0.05), Convert.ToInt32(tools.Width * 0.09), Convert.ToInt32(tools.Height * 0.8));
-            diagram3D.Font= font;
+            diagram3D.Font = font;
             tools.Controls.Add(diagram3D);
 
             Button histogram = new Button();
             histogram.BackColor = Color.White;
             histogram.Text = "Show Histogram";
             histogram.SetBounds(Convert.ToInt32(tools.Width * 0.815), Convert.ToInt32(tools.Height * 0.05), Convert.ToInt32(tools.Width * 0.09), Convert.ToInt32(tools.Height * 0.8));
-            histogram.Font= font;
+            histogram.Font = font;
             tools.Controls.Add(histogram);
 
             Button save = new Button();
             save.BackColor = Color.White;
             save.Text = "Save";
             save.SetBounds(Convert.ToInt32(tools.Width * 0.905), Convert.ToInt32(tools.Height * 0.05), Convert.ToInt32(tools.Width * 0.09), Convert.ToInt32(tools.Height * 0.8));
-            save.Font= font;
+            save.Font = font;
             tools.Controls.Add(save);
         }
+
+     
+
         private void intiTable()
         {
             DataGridView grid = new DataGridView();
-            grid.SetBounds(0,0,table.Width,Convert.ToInt32(table.Height*0.9));
+            grid.SetBounds(0, 0, table.Width, Convert.ToInt32(table.Height * 0.9));
             grid.ColumnCount = 1;
             grid.Columns[0].Name = "Empty";
             grid.Rows.Add("Empty Data");
@@ -128,13 +133,43 @@ namespace Systemy_Wspomagania_Decyzji
             grid.CellBorderStyle = DataGridViewCellBorderStyle.Single;
             grid.GridColor = Color.Black;
             grid.RowHeadersVisible = false;
-            grid.ScrollBars=ScrollBars.Both;
+            grid.ScrollBars = ScrollBars.Both;
             table.Controls.Add(grid);
         }
-        private void setTable(String[][] data)
+        private void setTable()
         {
-            throw new NotImplementedException();
+            table.Controls.Clear();
+            DataGridView grid = new DataGridView();
+            grid.SetBounds(0, 0, table.Width, Convert.ToInt32(table.Height * 0.9));
+            grid.ColumnCount = data[0].Length;
+            for (int i = 0; i < data[0].Length;i++) {
+                grid.Columns[i].Name = Tools.columnHeaders[i];
+            }
+            foreach (Object[] o in data)
+            {
+                grid.Rows.Add(o);
+            }
+            grid.AutoSizeRowsMode =
+            DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            grid.ColumnHeadersBorderStyle =
+                DataGridViewHeaderBorderStyle.Single;
+            grid.CellBorderStyle = DataGridViewCellBorderStyle.Single;
+            grid.GridColor = Color.Black;
+            grid.RowHeadersVisible = false;
+            grid.ScrollBars = ScrollBars.Both;
+            table.Controls.Add(grid);
+        }
+        private void loadtxt(object sender, EventArgs e)
+        {
+            data = Tools.loadTxt();
+            setTable();
+        }
+        private void loadExcel(object sender, EventArgs e)
+        {
+            data=Tools.loadFromExcel();
+            setTable();
         }
 
     }
 }
+
---
+++ Systemy Wspomagania Decyzji/Tools.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.VisualBasic.FileIO;
+
+
+namespace Systemy_Wspomagania_Decyzji
+{
+    internal class Tools
+    {
+        public static string[] columnHeaders;
+
+        public static Object[][] loadTxt()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.FilterIndex = 1;
+            string sFileName = "";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                sFileName = openFileDialog.FileName;
+
+
+
+                string[] delimiter = new string[] { "," };
+                System.Data.DataTable fileData = new System.Data.DataTable();
+                using (TextFieldParser pipeParser = new TextFieldParser(sFileName))
+                {
+                    pipeParser.SetDelimiters(delimiter);
+                    pipeParser.HasFieldsEnclosedInQuotes = true;
+                    columnHeaders = pipeParser.ReadFields();
+                    foreach (string column in columnHeaders)
+                    {
+                        if (!string.IsNullOrEmpty(column))
+                        {
+                            DataColumn datecolumn = new DataColumn(column);
+                            datecolumn.AllowDBNull = true;
+                            fileData.Columns.Add(datecolumn);
+                        }
+                    }
+                    while (!pipeParser.EndOfData)
+                    {
+                        string[] dataFields = pipeParser.ReadFields();
+                        fileData.Rows.Add(dataFields);
+                    }
+                }
+
+                List<string[]> ret = fileData.Rows.Cast<DataRow>().Select(row => fileData.Columns.Cast<DataColumn>()
+               .Select(col => Convert.ToString(row[col]))
+               .ToArray())
+               .ToList();
+                Object[][] list = new Object[ret.Count][];
+                int i = 0;
+                foreach (string[] str in ret)
+                {
+                    int j = 0;
+                    list[i] = new Object[str.Length];
+                    foreach (string s in str)
+                    {
+
+                        double m;
+                        bool isDouble = double.TryParse(s, NumberStyles.Number, CultureInfo.CreateSpecificCulture("en-US"), out m);
+                        if (isDouble)
+                        {
+                            list[i][j] = m;
+                        }
+                        else
+                        {
+                            DateTime d;
+                            bool isDate = DateTime.TryParse(s, out d);
+                            if (isDate)
+                            {
+                                list[i][j] = d;
+                            }
+                            else
+                            {
+                                list[i][j] = s;
+                            }
+                        }
+
+                        j++;
+                    }
+                    i++;
+                }
+
+                return list;
+
+            }
+            throw new ApplicationException();
+        }
+        public static Object[][] loadFromExcel()
+        {
+            Microsoft.Office.Interop.Excel.Application App = new Microsoft.Office.Interop.Excel.Application();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.FilterIndex = 1;
+            string sFileName = "";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                sFileName = openFileDialog.FileName;
+                Workbook wb = App.Workbooks.Open(sFileName);
+                Worksheet ws = wb.Worksheets[1];
+                int nrCol = 1;
+                for (; ; ) {
+                    if (ws.Cells[1, nrCol].Value == null) break;
+                    nrCol++;
+                }
+                var startCell = (Range)ws.Cells[1, 1];
+                var endCell = (Range)ws.Cells[1, nrCol-1];
+                var writeRange = ws.Range[startCell, endCell];
+                Object[,] headers = writeRange.Value2;
+                columnHeaders= new string[headers.Length];
+                int j = 0;
+                foreach (Object h in headers)
+                {
+                    columnHeaders[j] = h.ToString();
+                    j++;
+                }
+                int nrRow = 1;
+                for (; ; )
+                {
+                    if (ws.Cells[nrRow, 1].Value == null) break;
+                    nrRow++;
+                }
+                var startCell2 = (Range)ws.Cells[2, 1];
+                var endCell2 = (Range)ws.Cells[nrRow-1, nrCol - 1];
+                var writeRange2 = ws.Range[startCell2, endCell2];
+                Object[,] data = writeRange2.Value2;
+                Object[][] data2= new Object[nrRow-2][];
+                for (int it = 0; it<nrRow-2 ; it++) {
+                    Object[] o = new Object[nrCol-1];
+                    for (int jt = 0; jt < nrCol - 1; jt++)
+                    {
+                        o[jt] = data[it+1, jt+1];
+                    }
+                    data2[it] = o;
+                }
+                App.Quit();
+
+                return data2;
+            }
+            throw new ApplicationException();
+        }
+    }
+}
